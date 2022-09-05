7eab75d060f924508984ca993cd4e28f52f9f96f
Jarrett M. Nobles <nobles_jm@cobra.cs.mercer.edu>
2022-09-05 15:55:38 -0400

    almost done with sorting

--- java/Happy.java
+++ java/Happy.java
@@ -1,21 +1,28 @@
 import java.util.*;
 import java.util.Scanner;
 import java.util.TreeMap;
+
 //begin class
 public class Happy{
 
   //global variables
-  int firstNum,secondNum;
-public static  TreeMap <Double, Double> treeMap = new TreeMap<>();
+ public static  int firstNum,secondNum;
+
+  //hash map originally
+  public  static HashMap<Long, Integer> cycle = new HashMap<>();
+ //public  static HashSet<Long> cycle = new HashSet<>();
+  public  static TreeMap <Long, Integer> treeMap = new TreeMap<>(cycle);
   //takes user input, it also prints the first and second argument the user enters and it will flip them if first is bigger than second
-  public  void isHappy()
+  public  static void isHappy()
   {
   //first step is take user input
     Scanner in = new Scanner(System.in);
-    System.out.println("Enter the first argument:");
-    firstNum = in.nextInt();
-    System.out.println("Enter the second argument");
-    secondNum = in.nextInt();
+   //System.out.println("Enter the first argument:");
+    //firstNum = in.nextInt();
+    firstNum = 10;
+    //System.out.println("Enter the second argument");
+    //secondNum = in.nextInt();
+    secondNum = 30;
     ///this is where it will flip the values if the first is bigger than the second
     int temp;
     if(secondNum < firstNum)
@@ -27,13 +34,24 @@ public static  TreeMap <Double, Double> treeMap = new TreeMap<>();
    //print the arguments
     System.out.println("First Argument:" + firstNum);
     System.out.println("Second Argument:" + secondNum);
-
   }
-  
+  //print the reverse tree order
+    public static void printReverseTreeMap(TreeMap<Long,Integer> treeMap)
+    {
+        for(Long i : treeMap.descendingKeySet()){
+            //NavigableSet newTree = treeMap.descendingKeySet();
+            //Iterator itr1 = newTree.iterator();
+            //System.out.println(itr1.next());//prints 10 right now, either stores 1 from sum or num?
+            //System.out.println(itr1.next());
+            //Set <Long> i = treeMap.descendingKeySet();
+            System.out.println(i);
+
+
+        }
+    }
   //happy number calculations
-  public void happyCheck(TreeMap<Double, Double> map)
+  public static void happyCheck(TreeMap<Long, Integer> treeMap)
   {
-   // TreeMap <Double, Integer> treeMap = new TreeMap();
     int sum = 0; 
     int s, i, num;
     System.out.println("The happy numbers are: ");
@@ -41,38 +59,29 @@ public static  TreeMap <Double, Double> treeMap = new TreeMap<>();
     {
       num = i;
       sum = 0;
-      //System.out.println("test1"); 
       while (sum != 1 && sum != 4)
       {
-	//System.out.println("test2");
         sum = 0;
         while (num > 0 )
         {
-	  //System.out.println("test3");
           s = num % 10;
           sum += (s * s);
           num = num / 10;
         }
-        num = sum;
-	//System.out.println("test4");
+       num = sum;
       }
-      
-      if(num == 1)
+
+      //prints to the tree map
+      if(sum == 1)
       {
-	  
-	  
-	  
-   	  map.put((double)i,(double) sum);
-	    NavigableSet  newTree = treeMap.descendingKeySet();
-      System.out.println(newTree);
-	  
+        cycle.put((long)i,sum);
+        //System.out.println(cycle);
       }
-      
-         
     }
-   
+    treeMap.putAll(cycle);
+    printReverseTreeMap(treeMap);
   }
-  
+
   //main method
   public static void main(String[] args)
   {
@@ -80,7 +89,4 @@ public static  TreeMap <Double, Double> treeMap = new TreeMap<>();
     isHappy.isHappy();
     isHappy.happyCheck(treeMap);
   }
-
- 
-  
 }
