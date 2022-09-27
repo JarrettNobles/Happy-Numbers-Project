3bf12e77c40525eb4867bbbf076ddf2d430ec03c
Jarrett M. Nobles <nobles_jm@cobra.cs.mercer.edu>
2022-09-27 13:13:05 -0400

    pushed to github

--- Perl/happy.pl
+++ Perl/happy.pl
@@ -6,3 +6,12 @@ print "Enter the second argument: \n";
 my $secondNum = <STDIN>;
 print "First Argument: ",$firstNum,"\n";
 print "Second Argument: ",$secondNum,"\n";
+
+if($firstNum > $seconNum){
+	($firstNum,$secondNum) = ($secondNum,$firstNum);
+}
+
+print "First Argument: ",$firstNum,"\n";
+print "Second Argument: ",$secondNum,"\n";
+
+
