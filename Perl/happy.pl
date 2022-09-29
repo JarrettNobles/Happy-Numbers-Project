0fe9c4bfeb247c7200980bb896be310ae6919fc1
Jarrett M. Nobles <nobles_jm@cobra.cs.mercer.edu>
2022-09-29 19:27:20 -0400

    updates to Perl

--- Perl/happy.pl
+++ Perl/happy.pl
@@ -1,17 +1,41 @@
 #!/usr/bin/perl
+main();
+sub main{
+  
+  #user input section
+  print "Enter the first argument: \n";
+    $firstNum = <>;
+    chomp($firstNum);
+  print "Enter the second argument: \n";
+    $secondNum = <>;
+    chomp($secondNum);
+  swap();
+  print "First Argument: ",$firstNum,"\n";
+  print "Second Argument: ",$secondNum,"\n";
+  
+  #for loop to calculate norms
+  #for($i = $firstNum; $i <= $secondNum; $i++){
+    
+  #}
 
-print "Enter the first argument: \n";
-my $firstNum = <STDIN>;
-print "Enter the second argument: \n";
-my $secondNum = <STDIN>;
-print "First Argument: ",$firstNum,"\n";
-print "Second Argument: ",$secondNum,"\n";
-
-if($firstNum > $seconNum){
-	($firstNum,$secondNum) = ($secondNum,$firstNum);
 }
 
-print "First Argument: ",$firstNum,"\n";
-print "Second Argument: ",$secondNum,"\n";
+#swap sub function to swap the numbers if first number entered bigger than the 2nd
+sub swap{
+ if($firstNum > $secondNum){
+  	($firstNum,$secondNum) = ($secondNum,$firstNum);
+  }
+}
+#sub int sumDigitSquare($n){
+sub sumDigitSquare($n){
+  $sq=0;
+  while($n>0){
+    $digit = $n%10;
+    $sq += $digit * $digit;
+    $n = $n/10;
+  }
+  return $sq;
 
+}
 
+  
