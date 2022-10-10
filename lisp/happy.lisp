32044720da7182dc1ba76784ebee2d6ffb53e9a2
Jarrett M. Nobles <nobles_jm@cobra.cs.mercer.edu>
2022-10-10 13:25:45 -0400

    start of lisp

---
+++ lisp/happy.lisp
@@ -0,0 +1,9 @@
+#!/usr/bin/sbcl --script
+(write-line "Hello, World!")
+(defun main())
+(
+    (princ "Enter the First Argument: ")
+    (setq firstNum(read))
+    (princ "Enter the Second Argument: ")
+    (setq secondNum(read))
+)
