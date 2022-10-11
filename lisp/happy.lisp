a041eda01843206ff6667265a39665e3297f8ab8
Jarrett M. Nobles <nobles_jm@cobra.cs.mercer.edu>
2022-10-10 20:48:50 -0400

    more lisp updates

--- lisp/happy.lisp
+++ lisp/happy.lisp
@@ -2,8 +2,20 @@
 (write-line "Hello, World!")
 (defun main())
 (
+    ; user input 
     (princ "Enter the First Argument: ")
     (setq firstNum(read))
     (princ "Enter the Second Argument: ")
     (setq secondNum(read))
+
+    ; switch statement
+    (let ((value value)) 
+        (cond ((eql value '"XY") (print "XY")) 
+         ((eql value '"AB")))
+    
+    (princ "First Argument: ")
+    (write firstNum))
+    (princ "Second Argument: ")
+    (write secondNum))
+
 )
